b311872d91897cec58d656e858edd7f6f58c4302
gxmikvid <69439560+gxmikvid@users.noreply.github.com>
2022-10-26 11:19:02 +0200

    v2.0

---
+++ WindowsFormsFeladat/WindowsFormsFeladat/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsFeladat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Nev.Text) && Azonosito.Value > 0 && Nyelv.SelectedIndex > -1 && SzuletesiHely.SelectedIndex > -1)
+            {
+                MessageBox.Show("Sikeresen kitöltötte a kérdőívet!");
+            }
+            else
+            {
+                MessageBox.Show("Hiányos kitöltés!");
+            }
+        }
+    }
+}
